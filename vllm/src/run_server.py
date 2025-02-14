1f61d1c1dd50a2bc3cd6360f1c3fe54ed0f0bc45
JinSan-RM <yug6789@naver.com>
2025-02-14 17:15:35 +0900

    [vllm] vllm config

---
+++ vllm/src/run_server.py
@@ -0,0 +1,30 @@
+from vllm.entrypoints.openai.api_server import serve
+from vllm import SamplingParams
+import sys
+import os
+from config.vllm_config import VLLMConfig
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+def main():
+    config = VLLMConfig()
+    sampling_params=SamplingParams(
+            max_tokens=2000
+        )
+    serve(
+        model=config.model,
+        host=config.host,
+        port=config.port,
+        max_num_seqs=config.max_num_seqs,
+        max_batch_size=config.max_batch_size,
+        max_num_batched_tokens=config.max_num_batched_tokens,
+        gpu_memory_utilization=config.gpu_memory_utilization,
+        request_timeout=config.request_timeout,
+        max_waiting_tokens=config.max_waiting_tokens,
+        sampling_params=sampling_params
+        # quantization=config.quantization
+    )
+
+
+if __name__ == "__main__":
+    main()
