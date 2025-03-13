3824973734bb68204802c6a67e1ffeead8dc0dc7
shaw_weven <ycear0@gmail.com>
2025-03-13 14:35:40 +0900

    [convert_chatopenai] Dockerfile, yml에 수정사항항

--- langchain/src/openai/openai_api_call.py
+++ langchain/src/openai/openai_api_call.py
@@ -12,6 +12,14 @@ import asyncio
 # from logger.ve_logger import VeLogger
 # from openai import AsyncOpenAI
 
+from typing import Optional
+
+from pydantic import BaseModel, Field
+
+class Pydantic_Test(BaseModel):
+
+    question: str = Field(description="The question")
+    answer: str = Field(description="The answer")
 
 class OpenAIService:
     """This class handles openai requests."""
@@ -50,6 +58,10 @@ class OpenAIService:
         self.streaming = streaming
         callback_manager = AsyncCallbackManager([StreamingStdOutCallbackHandler()]) if streaming else CallbackManager([])
         
+        # NOTE : 여기에서 분기를 태울까? 괜찮아보이는데?
+        
+        print("openai_config.openai_api_base : ", openai_config.openai_api_base)
+        
         self.llm = OpenAI(
             model="/usr/local/bin/models/EEVE-Korean-Instruct-10.8B-v1.0",
             openai_api_key=openai_config.openai_api_key,
@@ -62,10 +74,37 @@ class OpenAIService:
         self.chat = ChatOpenAI(
             model="/usr/local/bin/models/EEVE-Korean-Instruct-10.8B-v1.0",
             openai_api_key=openai_config.openai_api_key,
+            # openai_api_base=openai_config.openai_api_base,
+            openai_api_base="http://vllm:8002/v1",
+            streaming=streaming,
+            max_tokens=2000
+        )
+
+        self.chatmsg = ChatOpenAI(
+            model="/usr/local/bin/models/EEVE-Korean-Instruct-10.8B-v1.0",
+            openai_api_key=openai_config.openai_api_key,
             openai_api_base=openai_config.openai_api_base,
             streaming=streaming,
             max_tokens=2000
         )
+        
+        self.chatguided = ChatOpenAI(
+            model="/usr/local/bin/models/EEVE-Korean-Instruct-10.8B-v1.0",
+            openai_api_key=openai_config.openai_api_key,
+            openai_api_base="http://vllm:8002/v1",
+            extra_body={"guided_json": {
+                "type": "object",
+                "properties": {"question": {"type": "string"}, "answer": {"type": "string"}},
+                "required": ["question", "answer"]
+            }}
+        )
+        
+        # # NOTE : 임시 테스트용용
+        # self.chatgpt = ChatOpenAI(
+        #     model="gpt-4o",
+        #     openai_api_key="",
+        #     openai_api_base="https://api.openai.com/v1"
+        # )        
         
     async def completions(self, **kwargs):
         max_tokens = kwargs.get("max_tokens", self.llm.max_tokens)
@@ -74,9 +113,94 @@ class OpenAIService:
             if self.streaming:
                 response = await self.stream_completion(**kwargs)
             else:
-                response = await asyncio.to_thread(self.llm.generate, [kwargs['prompt']], max_tokens=max_tokens)
+                # NOTE : 여기서 arguments 체크해보기
+                print("generate 할 때 여기가 돌아가는게 맞는건가??")
+                # print("kwargs : ", kwargs)
+                # print("[kwargs['prompt']] : ", [kwargs['prompt']])
+                
+                test_data = [
+                    (
+                        "system",
+                        """
+                                    You are a professional in business plan writing. 
+            You are provided with user input in the form of a sentence or paragraph. 
+            Your task is to write a narrative paragraph to assist in creating a business plan based on this input. 
+            Follow these instructions precisely:
+
+            #### INSTRUCTIONS ####
+            
+            STEP 1. Identify and include key information from the user input, such as below. If the usr_input is not enough, you can fill it yourself.
+                1) BUSINESS ITEM: Specific product or service details
+                2) SLOGAN OR CATCH PHRASE: A sentence expressing the company's main vision or ideology
+                3) TARGET CUSTOMERS: Characteristics and needs of the major customer base
+                4) CORE VALUE PROPOSITION: Unique value provided to customers
+                5) PRODUCT AND SERVICE FEATURES: Main functions and advantages
+                6) BUSINESS MODEL: Processes that generate profits by providing differentiated value
+                7) PROMOTION AND MARKETING STRATEGY: How to introduce products or services to customers            
+            STEP 2. Develop the business plan narrative step-by-step using only the keywords and details from the user input. Do not expand the scope beyond the provided content.
+            STEP 3. Write a paragraph of 1000 to 1500 characters to ensure the content is detailed and informative.
+            STEP 4. Avoid repeating the same content to meet the character limit. Use varied expressions and vocabulary to enrich the narrative.
+            STEP 5. Do not include repeated User-Assistant interactions or unnecessary filler. End the output naturally after completing the narrative.
+            STEP 6. Ensure the text is free of typos and grammatical errors.
+            STEP 7. Output only the final business plan narrative text. Do not include tags (e.g., [SYSTEM], <|eot_id|>), JSON formatting (e.g., {{Output: "..."}}), or any metadata in the output.
+            STEP 8. 출력은 반드시 **한국어**로 해.
+                        """
+                    ),
+                    ("human", "abc 우리 기업의 이름은 '제주 귤 농장'입니다. 참고해서 작업해주세요.")
+                ]
+                
+                test_data2 = "Respond **only** with a valid JSON object containing 'question' and 'answer' keys, no additional text. Example: {\"question\": \"한국의 수도는 어디인가요?\", \"answer\": \"서울\"}"
+                
+                test_data3 = [
+                                (
+                                    "system",
+                                    "You are a helpful translator. Translate the user sentence to Korean.",
+                                ),
+                                ("human", "can you translate this sentence?"),
+                            ]
+                
+                test_data3_2 = [
+                    SystemMessage(content="You are a helpful translator. Translate the user sentence to Korean."),
+                    HumanMessage(content="너가 이전에 했던 대답 알려줘"),
+                ]                
+                
+                # structured_llm_gpt = self.chatgpt.with_structured_output(Pydantic_Test)
+                # response = structured_llm_gpt.invoke(test_data2)
+                # response = await asyncio.to_thread(structured_llm_gpt.invoke, input=test_data2)
+                
+                
+                # structured_llm = self.chat.with_structured_output(Pydantic_Test)
+                # response = await structured_llm.invoke(test_data2)     
+                           
+                # structured_llm = self.chat.with_structured_output(Pydantic_Test)
+                # response = await structured_llm.invoke(test_data3)
+                # response = await asyncio.to_thread(structured_llm.invoke, input=test_data3)
+                
+                # response = await asyncio.to_thread(structured_llm.invoke, input=test_data)
+                # response = await asyncio.to_thread(self.chat.invoke, input=test_data2)
+                # response = await asyncio.to_thread(self.llm.invoke, input=test_data2)
+                # response = await asyncio.to_thread(self.llm.generate, prompts=[test_data2])
+                # response = await asyncio.to_thread(self.llm.generate, [kwargs['prompt']], max_tokens=max_tokens)
+                
+                test_data4 = "Return a JSON object with key 'random_ints' and a value of 10 random ints in [0-99]"
+                # response = await asyncio.to_thread(self.chatmsg.invoke, input=test_data3)
+                # NOTE : 다음 메시지가 나오며 안됨. OpenAI API call failed: 'RunnableBinding' object is not callable
+                # jsonmode_chat = self.chat.bind(response_format={"type": "json_object"})
+                # response = await asyncio.to_thread(jsonmode_chat.invoke, input=test_data)
+                
+                response = await asyncio.to_thread(self.chatguided.invoke, input=test_data3_2)
+                
+                # structured_llm_gpt = self.chatguided.with_structured_output(Pydantic_Test)
+                # response = await asyncio.to_thread(structured_llm_gpt.invoke, input=test_data3_2)
+                
+                print("TYPE_어떤게 반환되려나? : ", type(response))
+                print("어떤게 반환되려나? : ", response)
+                print("어떤게 반환되려나?_type(response.content) : ", type(response.content))
+                print("어떤게 반환되려나?_response.content : ", response.content)
+                
             # print(f"Completions response: {response}")
             return response
+        
         except Exception as e:
             print(f"OpenAI API call failed: {str(e)}")
             raise
--- vllm/src/run_server.py
+++ vllm/src/run_server.py
@@ -1,33 +1,64 @@
-from vllm.entrypoints.openai.api_server import serve
+from vllm.entrypoints.openai.api_server import run_server
 from vllm import SamplingParams
 import sys
 import os
+
+import argparse
+import asyncio
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from config.vllm_config import VLLMConfig
 # os.environ["VLLM_LOGGING_LEVEL"] = "DEBUG"
 # os.environ["CUDA_LAUNCH_BLOCKING"] = "1"
 # os.environ["NCCL_DEBUG"] = "TRACE"
 # os.environ["VLLM_TRACE_FUNCTION"] = "1"
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 
 def main():
-    config = VLLMConfig()
-    sampling_params=SamplingParams(
-            # max_tokens=2000
-        )
-    serve(
-        model=config.model,
-        host=config.host,
-        port=config.port,
-        max_num_seqs=config.max_num_seqs,
-        max_batch_size=config.max_batch_size,
-        max_num_batched_tokens=config.max_num_batched_tokens,
-        gpu_memory_utilization=1.0,
-        request_timeout=config.request_timeout,
-        max_waiting_tokens=config.max_waiting_tokens,
-        sampling_params=sampling_params,
-        quantization=config.quantization
-    )
-
-
+    config = VLLMConfig()  # 설정 로드
+    
+    print("vllm container main test1111111111111111")  # 디버깅 출력
+    
+    # sampling_params = SamplingParams(
+    #     max_tokens=2000
+    # )
+    # NOTE : serve는 CLI로 실행 시 사용되는 명령어
+    # ※ https://docs.vllm.ai/en/latest/serving/openai_compatible_server.html
+    # serve(
+    #     model="/usr/local/bin/models/EEVE-Korean-Instruct-10.8B-v1.0",  # Dockerfile에서 복사된 경로
+    #     host=config.host,
+    #     port=config.port,
+    #     max_num_seqs=config.max_num_seqs,
+    #     max_batch_size=config.max_batch_size,
+    #     max_num_batched_tokens=config.max_num_batched_tokens,
+    #     gpu_memory_utilization=1.0,
+    #     request_timeout=config.request_timeout,
+    #     max_waiting_tokens=config.max_waiting_tokens,
+    #     sampling_params=sampling_params,
+    #     quantization=config.quantization,
+    #     dtype="float16",
+    #     max_model_len=4096
+    # )
+    
+    # parser = argparse.ArgumentParser(description="vLLM OpenAI API Server")
+    # parser.add_argument("--model", default=config.model, type=str)
+    # parser.add_argument("--host", default=config.host, type=str)
+    # parser.add_argument("--port", default=config.port, type=int)
+    # parser.add_argument("--max-num-seqs", default=config.max_num_seqs, type=int)
+    # parser.add_argument("--max-batch-size", default=config.max_batch_size, type=int)
+    # parser.add_argument("--max-num-batched-tokens", default=config.max_num_batched_tokens, type=int)
+    # parser.add_argument("--gpu-memory-utilization", default=1.0, type=float)
+    # parser.add_argument("--request-timeout", default=config.request_timeout, type=int)
+    # parser.add_argument("--max-waiting-tokens", default=config.max_waiting_tokens, type=int)
+    # parser.add_argument("--quantization", default=config.quantization, type=str)
+    # parser.add_argument("--dtype", default="float16", type=str)
+    # parser.add_argument("--max-model-len", default=4096, type=int)
+    
+    # args = parser.parse_args()
+    # asyncio.run(run_server(args))
+    
+    
 if __name__ == "__main__":
+    print("vllm container main test222222222222")
     main()
+    print("vllm container main test3333333333333")
